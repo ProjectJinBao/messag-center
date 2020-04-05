876fe4168a587ec36477c93f0a52d420014572ea
yujing <362639663@qq.com>
2020-04-05 21:06:29 +0800

    create

--- service/svc_sendmsg.py
+++ service/svc_sendmsg.py
@@ -16,21 +16,18 @@ def svc_template_sendmsg(content_name, robot_key):
     #获取发送url和content
     rsl=db_sendmsg(content_name=content_name, robot_key=robot_key)
     # post_url = rsl.get("url")
-    project = rsl.get("project")
-    sprint = rsl.get("sprint")
     content1 = rsl.get("content")
-    content = ContentHandle().conten_handle_template(content1)
-
     key = robot_key
     msgtype = rsl.get("msgtype")
     if msgtype == "text":
-        QYWX_Robot(key).send_text(content)
+        QYWX_Robot(key).send_text(content1)
     elif msgtype == "markdown":
+        content = ContentHandle().conten_handle_template(content1)
         QYWX_Robot(key).send_markdown(content)
     elif msgtype == "images":
-        QYWX_Robot(key).send_markdown(content)
+        QYWX_Robot(key).send_markdown(content1)
     elif msgtype == "news":
-        QYWX_Robot(key).send_markdown(content)
+        QYWX_Robot(key).send_markdown(content1)
     else:
         raise IsNotExist(title='不支持该格式的消息', detail=f'不支持【{msgtype}】的格式')
 
