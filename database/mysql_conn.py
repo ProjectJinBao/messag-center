f5ec9a79e7e4936edd230b23d4c43b3e30cbe027
yujing <362639663@qq.com>
2020-06-09 10:16:09 +0800

    create

--- database/mysql_conn.py
+++ database/mysql_conn.py
@@ -1,5 +1,6 @@
 
 from pony.orm import Database
+import os
 from pony.orm import (Json, PrimaryKey, Required, db_session, select, desc, Set, get, Optional)
 '''
 Pony规定与数据库进行交互的代码必须在数据库会话中工作
@@ -11,13 +12,26 @@ Pony规定与数据库进行交互的代码必须在数据库会话中工作
 清除缓存。
 '''
 
-db = Database(provider = "mysql", host = '10.10.30.59',
-                     port = 3307,
-                     user = 'root',
-                     password = '123456',
-                     database = 'message_center',
-                     charset = "utf8mb4"
+
+
+db = Database(provider = "mysql",
+              host = os.getenv('HOST') if os.getenv('MY_HOST') else '10.10.30.59',
+              port = os.getenv('Port') or 3307,
+              user = os.getenv('USER') if os.getenv('MY_USER') else 'root',
+              password = os.getenv('PASSWORD') or '123456',
+              database = os.getenv('DB') or 'message_center',
+              charset = "utf8mb4"
               )
+
+
+
+# db = Database(provider = "mysql", host = '10.10.30.59',
+#                      port = 3307,
+#                      user = 'root',
+#                      password = '123456',
+#                      database = 'message_center',
+#                      charset = "utf8mb4"
+#               )
 
 
 
@@ -28,7 +42,8 @@ db = Database(provider = "mysql", host = '10.10.30.59',
 #                      user = 'root',
 #                      passwd = '123456',
 #                      db = 'message_center' )
-# cur = conn.cursor()
+# cur = conn.cursor():q
+
 # cur.execute("select * from info")
 
 
